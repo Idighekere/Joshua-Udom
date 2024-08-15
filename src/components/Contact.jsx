b17f4ece2faf2f32f458eda8353e2b085ed73d67
Idighekere Udo <101113870+Idighekere@users.noreply.github.com>
2024-08-15 17:38:04 +0100

    Update Contact.jsx
    Fix: made icons to be displayed in a single column on small screen size

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
             <p className='text-xl text-center' data-aos="fade-left">Get in touch with me for full-time job opportunities, freelance, design advice, or simply to say hello 😃
                 .</p>
 
-            <div className='grid grid-rowss-4 grid-cols-2 md:grid-cols-4 gap-3 sm:grid-cols-3 justify-center py-7' >
+            <div className='grid grid-rowss-4 grid-cols-1 md:grid-cols-3 gap-3 sm:grid-cols-2 lg:grid-cols-4 justify-center py-7' >
                 {
                     socialLinks.map(link => (<span key={link.name} className=''><Link to={link.link} className={`flex gap-3 justify-around items-center border-2 border-white/90 rounded-full mr-3 w-auto px-2 py-2 md:px-4 md:py-1 hover:bg-white/80  drop-shadow-lg ${link.color} `} data-aos="zoom-out" target='_blank'>
                         <img src={link.icon} alt={link.name} width="10px" className='w-10' /> <p className='hiddenz md:block'>{link.name}</p></Link>
